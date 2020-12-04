0452494a05745cca88f8c4b6364a344491ae53ce
andresllinas <andreslli1707@gmail.com>
2020-12-03 19:07:29 -0500

    cambios comentarios

--- partials/gauge_data.php
+++ partials/gauge_data.php
@@ -1,4 +1,6 @@
+
 <script type="text/javascript">
+
 
         /**  Script creado por:
         Andres Llinas
@@ -6,43 +8,42 @@
         Daniel Bonilla
         */
         
-
-
   google.charts.load('current', {
     'packages': ['gauge']
   });
   google.charts.setOnLoadCallback(drawChart);
-´
-  /** Funcion que consigue los datos mediante comandos linux y los devuelve con el formato requerido */
+
   function drawChart() {
 
+    /** Funcion que retorna los datos requeridos del procesador en formato js */
     var data = google.visualization.arrayToDataTable([
       ['Label', 'Value'],
    
       <?php
 
-      #Calculo de numero de procesadores y porcentaje de uso del procesador en diferentes tiempos
+      #Se obtienen las cifras de uso y numero de nuclos mediante comandos linux
       exec("nproc", $numero_nucleos);
       exec(" uptime|awk '{print $8,$9,$10}'", $av);
 
+      #con el substring luego se reemplaza las otras comas por puntos y se calcula el porcentaje
       $data = explode(" ", $av[0]);
       $av1 = substr($data[0], 0, strlen($data[0]) - 1);
       $av2 = substr($data[1], 0, strlen($data[1]) - 1);
       $av3 = $data[2];
 
-      #con el substring luego se reemplaza las otras comas por puntos y se calcula el porcentaje
       $av3 = (str_replace(",", ".", $av3) / $numero_nucleos[0]) * 100;
       $av2 = (str_replace(",", ".", $av2) / $numero_nucleos[0]) * 100;
       $av1 = (str_replace(",", ".", $av1) / $numero_nucleos[0]) * 100;
 
       
-      #luego se imprime el formato del arreglo tipo javascript
+      #finalmente se formatean los datos conseguidos en tipo javascript para ser impresos mediante echo
       echo "['1 Min',".$av1."],";
       echo "['5 Min',".$av2."],";
       echo "['15 Min',".$av3."],";
       ?>
     ]);
 
+    /**Configuraciones graficas para las graficas de google charts */
     var options = {
       width: 400,
       height: 110,
@@ -53,6 +54,7 @@
       minorTicks: 5
     };
 
+    /**vinculacion de los metodos del back con el elemento html donde va a ir la grafica */
     var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
 
     chart.draw(data, options);
